da3b23be51b331669e5dd2f6118df7ecfcbc0d93
Phahladira-Moja <phahladira@patala.co.za>
2023-06-09 22:49:02 +0200

    Created the searchbar components

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,17 @@
 import Hero from "./Hero";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, Navbar, Footer, CustomButton };
+export {
+  Hero,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  CustomButton,
+  SearchManufacturer,
+};
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   btnTyp?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
