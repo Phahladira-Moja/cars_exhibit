efd89af73602a1366ec144332bf7d847e39c40d9
Phahladira-Moja <phahladira@patala.co.za>
2023-06-12 05:01:39 +0200

    Added search function to button and added custom button component. Added search filters to API request

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@ import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 import Image from "next/image";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+// NB in Next you can auto get the search params staight from props
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || "2022",
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || "10",
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
-  console.log(allCars);
   return (
     <main className="overflow-hidden">
       <Hero />
